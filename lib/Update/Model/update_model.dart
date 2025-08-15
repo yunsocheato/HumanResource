a2bcd473583d43270077d1512a427dceb38b7c6d
yunsocheato <ysocheato@gmail.com>
2025-08-15 22:31:18 +0700

    Update button update in project

--- lib/Update/Model/update_model.dart
+++ lib/Update/Model/update_model.dart
@@ -9,15 +9,4 @@ class UpdateModelGithub {
     this.branch = 'master',
 });
 
-  factory UpdateModelGithub.fromJson(Map<String, dynamic> json) => UpdateModelGithub(
-    repoOwner: json['repo_owner'],
-    repoName: json['repo_name'],
-    branch: json['branch'],
-  );
-
-  Map<String, dynamic> toJson() => {
-    'repo_owner': repoOwner,
-    'repo_name': repoName,
-    'branch': branch,
-  };
 }
