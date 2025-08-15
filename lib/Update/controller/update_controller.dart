8b728d8f9b44eb5094e470d7f4dc61d4ed082a8a
yunsocheato <ysocheato@gmail.com>
2025-08-15 23:03:17 +0700

    Update project

--- lib/Update/controller/update_controller.dart
+++ lib/Update/controller/update_controller.dart
@@ -2,63 +2,91 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import '../API/update_api.dart';
+import '../Model/update_model.dart';
 
-class UpdateController extends GetxController {
-  final hasNewUpdate = false.obs;
+class UpdateController extends GetxController{
+  final buttonUpdate = true.obs;
   final isLoading = false.obs;
+  final hasnewUpdate = false.obs;
+  final RxString filecontent = ''.obs ;
+  final RxList<Map<String, dynamic>> allFiles = <Map<String, dynamic>>[].obs;
 
-  String? latestCommitSha;
 
-  final repoOwner = "yunsocheato";
-  final repoName = "HumanResource";
-  final branch = "master";
+  var Username = 'yunsocheato'.obs;
 
-  final githubToken = "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";
+  var model = UpdateModelGithub(
+    repoOwner: 'yunsocheato',
+    repoName: 'HumanResource',
+    branch: 'master',
+  );
+  late UpdateGitHubAPI gitHubAPI;
+  String? lastcommitsha;
 
   @override
   void onInit() {
     super.onInit();
-    checkForUpdate();
-
-    Timer.periodic(const Duration(seconds: 60), (_) => checkForUpdate());
+    gitHubAPI = UpdateGitHubAPI(
+      githubToken: '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'
+    );
+    CheckForUpdate();
+    Timer.periodic(Duration(seconds: 60), (_) => CheckForUpdate());
   }
 
-  Future<void> checkForUpdate() async {
+  Future<void> CheckForUpdate() async {
+    final url = Uri.parse('https://api.github.com/repos/${model.repoOwner}/${model.repoName}/commits/${model.branch}');
     try {
-      final url = Uri.parse(
-        "https://api.github.com/repos/$repoOwner/$repoName/commits/$branch",
-      );
-
-      final headers = {
-        "Accept": "application/vnd.github+json",
-        if (githubToken.isNotEmpty) "Authorization": "Bearer $githubToken",
-      };
-
-      final response = await http.get(url, headers: headers);
-
+      final response = await http.get(url);
       if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-
-        final latestSha = data['sha'];
-
-        if (latestCommitSha != null && latestSha != latestCommitSha) {
-          hasNewUpdate.value = true;
+        final List data = jsonDecode(response.body);
+        if (data.isNotEmpty) {
+          final latestSha = data[0]['sha'];
+          print('Latest SHA: $latestSha, Last SHA: $lastcommitsha');
+          if (latestSha != lastcommitsha) {
+            lastcommitsha = latestSha;
+            hasnewUpdate.value = true;
+            print('New update detected!');
+          }
         }
-
-        latestCommitSha = latestSha;
       } else {
-        print("GitHub API error: ${response.statusCode}");
+        print('GitHub API error: ${response.statusCode}');
       }
     } catch (e) {
-      print("Update check failed: $e");
+      print('Error checking commits: $e');
     }
   }
 
-  Future<void> fetchAllFiles() async {
+  Future<void> fetchAllFiles([String path = '']) async {
     isLoading.value = true;
-    await Future.delayed(const Duration(seconds: 2));
-    isLoading.value = false;
-
-    hasNewUpdate.value = false;
+    hasnewUpdate.value = false;
+    final url = Uri.parse(
+        'https://api.github.com/repos/${model.repoOwner}/${model.repoName}/contents/$path?ref=${model.branch}');
+    try {
+      final response = await http.get(url, headers: {
+        'Authorization':
+        'token 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'
+      });
+      if (response.statusCode == 200) {
+        final List data = jsonDecode(response.body);
+        for (var item in data) {
+          if (item['type'] == 'file') {
+            allFiles.add({
+              'path': item['path'],
+              'download_url': item['download_url'],
+            });
+          } else if (item['type'] == 'dir') {
+            await fetchAllFiles(item['path']);
+          }
+        }
+        Get.snackbar('Update', 'Fetched all files from GitHub',
+            snackPosition: SnackPosition.TOP);
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to fetch files: $e',
+          snackPosition: SnackPosition.TOP);
+    } finally {
+      isLoading.value = false;
+      update();
+    }
   }
 }
