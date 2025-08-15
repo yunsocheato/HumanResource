7b94844c4a9e5db79868327bc60f544af39a99ed
yunsocheato <ysocheato@gmail.com>
2025-08-15 22:51:12 +0700

    Update project

--- lib/Update/view/update_screen.dart
+++ lib/Update/view/update_screen.dart
@@ -16,7 +16,7 @@ class UpdateButtonScreen extends GetView<UpdateController> {
     final controller = Get.find<UpdateController>();
     final isMobile = Get.width < 600;
     return Obx(() {
-      if (controller.hasnewUpdate.value) {
+      if (controller.hasNewUpdate.value) {
         return ElevatedButton(
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.black12,
